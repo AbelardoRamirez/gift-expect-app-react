5d3e9cfb88f7762cd78a0d9a50e7dc2f3504b8ea
Jesus Abelardo Ramirez Zambrano <ja.ramirez.zambrano@gmail.com>
2020-10-20 13:16:55 -0500

    Creacion de GitHub Pages & Habilitar mi Build de produccion en Docs

---
+++ docs/precache-manifest.f6f0182a5f2a81d13ff406a6abca3dd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f020ee77ea1d0fc01aa713df2a1281",
+    "url": "/index.html"
+  },
+  {
+    "revision": "651ec032b036fc79e5be",
+    "url": "/static/css/2.da1bc806.chunk.css"
+  },
+  {
+    "revision": "651ec032b036fc79e5be",
+    "url": "/static/js/2.1b1e8b94.chunk.js"
+  },
+  {
+    "revision": "1a17c67f4311437e33b522bcde67da07",
+    "url": "/static/js/2.1b1e8b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62225f3e1eaf380e73ad",
+    "url": "/static/js/main.d160d110.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
